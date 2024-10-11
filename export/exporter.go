b4e5c76692f8b8f089645e11d2715f1872132c1f
hovsep <avakyan.hovsep@yandex.ru>
2024-10-12 02:57:44 +0300

    [WIP] Export with cycles

--- export/exporter.go
+++ export/exporter.go
@@ -1,8 +1,15 @@
 package export
 
-import "github.com/hovsep/fmesh"
+import (
+	"github.com/hovsep/fmesh"
+	"github.com/hovsep/fmesh/cycle"
+)
 
 // Exporter is the common interface for all formats
 type Exporter interface {
+	// Export returns f-mesh representation in some format
 	Export(fm *fmesh.FMesh) ([]byte, error)
+
+	// ExportWithCycles returns representations of f-mesh during multiple cycles
+	ExportWithCycles(fm *fmesh.FMesh, cycles cycle.Collection) ([][]byte, error)
 }
