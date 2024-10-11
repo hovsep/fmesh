f1951559627a0634d6577d37810cbff90f89cb81
hovsep <avakyan.hovsep@yandex.ru>
2024-10-12 02:57:44 +0300

    [WIP] Export with cycles

--- export/dot.go
+++ export/dot.go
@@ -4,7 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/hovsep/fmesh"
-	"github.com/hovsep/fmesh/component"
+	fmeshcomponent "github.com/hovsep/fmesh/component"
+	"github.com/hovsep/fmesh/cycle"
 	"github.com/hovsep/fmesh/port"
 	"github.com/lucasepe/dot"
 )
@@ -12,7 +13,11 @@ import (
 type dotExporter struct {
 }
 
-const nodeIDLabel = "export/dot/id"
+const (
+	nodeIDLabel    = "export/dot/id"
+	portKindInput  = "input"
+	portKindOutput = "output"
+)
 
 func NewDotExporter() Exporter {
 	return &dotExporter{}
@@ -36,11 +41,52 @@ func (d *dotExporter) Export(fm *fmesh.FMesh) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
+// ExportWithCycles returns multiple graphs showing the state of the given f-mesh in each activation cycle
+func (d *dotExporter) ExportWithCycles(fm *fmesh.FMesh, cycles cycle.Collection) ([][]byte, error) {
+	if len(fm.Components()) == 0 {
+		return nil, nil
+	}
+
+	if len(cycles) == 0 {
+		return nil, nil
+	}
+
+	results := make([][]byte, len(cycles))
+
+	for cycleNumber, c := range cycles {
+		graphForCycle, err := buildGraphForCycle(fm, c, cycleNumber)
+		if err != nil {
+			return nil, err
+		}
+
+		buf := new(bytes.Buffer)
+		graphForCycle.Write(buf)
+
+		results[cycleNumber] = buf.Bytes()
+	}
+
+	return results, nil
+}
+
 // buildGraph returns a graph representing the given f-mesh
 func buildGraph(fm *fmesh.FMesh) (*dot.Graph, error) {
 	mainGraph := getMainGraph(fm)
 
-	addComponents(mainGraph, fm.Components())
+	addComponents(mainGraph, fm.Components(), nil)
+
+	err := addPipes(mainGraph, fm.Components())
+	if err != nil {
+		return nil, err
+	}
+	return mainGraph, nil
+}
+
+func buildGraphForCycle(fm *fmesh.FMesh, activationCycle *cycle.Cycle, cycleNumber int) (*dot.Graph, error) {
+	mainGraph := getMainGraph(fm)
+
+	addCycleInfo(mainGraph, activationCycle, cycleNumber)
+
+	addComponents(mainGraph, fm.Components(), activationCycle.ActivationResults())
 
 	err := addPipes(mainGraph, fm.Components())
 	if err != nil {
@@ -50,7 +96,7 @@ func buildGraph(fm *fmesh.FMesh) (*dot.Graph, error) {
 }
 
 // addPipes adds pipes representation to the graph
-func addPipes(graph *dot.Graph, components component.Collection) error {
+func addPipes(graph *dot.Graph, components fmeshcomponent.Collection) error {
 	for _, c := range components {
 		for _, srcPort := range c.Outputs() {
 			for _, destPort := range srcPort.Pipes() {
@@ -58,15 +104,15 @@ func addPipes(graph *dot.Graph, components component.Collection) error {
 				// so we use the label we added earlier
 				destPortID, err := destPort.Label(nodeIDLabel)
 				if err != nil {
-					return fmt.Errorf("failed to add pipe: %w", err)
+					return fmt.Errorf("failed to add pipe to port: %s : %w", destPort.Name(), err)
 				}
-				// Clean up and leave the f-mesh as it was before export
+				// Delete label, as it is not needed anymore
 				destPort.DeleteLabel(nodeIDLabel)
 
 				// Any source port in any pipe is always output port, so we can build its node ID
-				srcPortNode := graph.FindNodeByID(getPortID(c.Name(), "output", srcPort.Name()))
+				srcPortNode := graph.FindNodeByID(getPortID(c.Name(), portKindOutput, srcPort.Name()))
 				destPortNode := graph.FindNodeByID(destPortID)
-				graph.Edge(srcPortNode, destPortNode).Attr("minlen", 3)
+				graph.Edge(srcPortNode, destPortNode).Attr("minlen", "3")
 			}
 		}
 	}
@@ -74,28 +120,32 @@ func addPipes(graph *dot.Graph, components component.Collection) error {
 }
 
 // addComponents adds components representation to the graph
-func addComponents(graph *dot.Graph, components component.Collection) {
+func addComponents(graph *dot.Graph, components fmeshcomponent.Collection, activationResults fmeshcomponent.ActivationResultCollection) {
 	for _, c := range components {
 		// Component
-		componentSubgraph := getComponentSubgraph(graph, c)
-		componentNode := getComponentNode(componentSubgraph, c)
+		var activationResult *fmeshcomponent.ActivationResult
+		if activationResults != nil {
+			activationResult = activationResults.ByComponentName(c.Name())
+		}
+		componentSubgraph := getComponentSubgraph(graph, c, activationResult)
+		componentNode := getComponentNode(componentSubgraph, c, activationResult)
 
 		// Input ports
 		for _, p := range c.Inputs() {
-			portNode := getPortNode(c, p, "input", componentSubgraph)
+			portNode := getPortNode(c, p, portKindInput, componentSubgraph)
 			componentSubgraph.Edge(portNode, componentNode)
 		}
 
 		// Output ports
 		for _, p := range c.Outputs() {
-			portNode := getPortNode(c, p, "output", componentSubgraph)
+			portNode := getPortNode(c, p, portKindOutput, componentSubgraph)
 			componentSubgraph.Edge(componentNode, portNode)
 		}
 	}
 }
 
 // getPortNode creates and returns a node representing one port
-func getPortNode(c *component.Component, port *port.Port, portKind string, componentSubgraph *dot.Graph) *dot.Node {
+func getPortNode(c *fmeshcomponent.Component, port *port.Port, portKind string, componentSubgraph *dot.Graph) *dot.Node {
 	portID := getPortID(c.Name(), portKind, port.Name())
 
 	//Mark ports to be able to find their respective nodes later when adding pipes
@@ -110,7 +160,7 @@ func getPortNode(c *component.Component, port *port.Port, portKind string, compo
 }
 
 // getComponentSubgraph creates component subgraph and returns it
-func getComponentSubgraph(graph *dot.Graph, component *component.Component) *dot.Graph {
+func getComponentSubgraph(graph *dot.Graph, component *fmeshcomponent.Component, activationResult *fmeshcomponent.ActivationResult) *dot.Graph {
 	componentSubgraph := graph.NewSubgraph()
 	componentSubgraph.
 		NodeBaseAttrs().
@@ -123,17 +173,52 @@ func getComponentSubgraph(graph *dot.Graph, component *component.Component) *dot
 		Attr("bgcolor", "lightgrey").
 		Attr("margin", "20")
 
+	// In cycle
+	if activationResult != nil {
+		switch activationResult.Code() {
+		case fmeshcomponent.ActivationCodeOK:
+			componentSubgraph.Attr("bgcolor", "green")
+		case fmeshcomponent.ActivationCodeNoInput:
+			componentSubgraph.Attr("bgcolor", "yellow")
+		case fmeshcomponent.ActivationCodeNoFunction:
+			componentSubgraph.Attr("bgcolor", "gray")
+		case fmeshcomponent.ActivationCodeReturnedError:
+			componentSubgraph.Attr("bgcolor", "red")
+		case fmeshcomponent.ActivationCodePanicked:
+			componentSubgraph.Attr("bgcolor", "pink")
+		case fmeshcomponent.ActivationCodeWaitingForInputsClear:
+			componentSubgraph.Attr("bgcolor", "blue")
+		case fmeshcomponent.ActivationCodeWaitingForInputsKeep:
+			componentSubgraph.Attr("bgcolor", "purple")
+		default:
+		}
+	}
+
 	return componentSubgraph
 }
 
-// getComponentNodeCreate creates component node and returns it
-func getComponentNode(componentSubgraph *dot.Graph, component *component.Component) *dot.Node {
+// getComponentNode creates component node and returns it
+func getComponentNode(componentSubgraph *dot.Graph, component *fmeshcomponent.Component, activationResult *fmeshcomponent.ActivationResult) *dot.Node {
 	componentNode := componentSubgraph.Node()
-	componentNode.Attr("label", "𝑓")
+	label := "𝑓"
+
 	if component.Description() != "" {
-		componentNode.Attr("label", component.Description())
+		label = component.Description()
 	}
+
+	if activationResult != nil {
+
+		if activationResult.Error() != nil {
+			errorNode := componentSubgraph.Node()
+			errorNode.
+				Attr("shape", "note").
+				Attr("label", activationResult.Error().Error())
+			componentSubgraph.Edge(componentNode, errorNode)
+		}
+	}
+
 	componentNode.
+		Attr("label", label).
 		Attr("color", "blue").
 		Attr("shape", "rect").
 		Attr("group", component.Name())
@@ -154,19 +239,58 @@ func getMainGraph(fm *fmesh.FMesh) *dot.Graph {
 	return graph
 }
 
+// addDescription adds f-mesh description to graph
 func addDescription(graph *dot.Graph, description string) {
-	descriptionSubgraph := graph.NewSubgraph()
-	descriptionSubgraph.
+	subgraph := graph.NewSubgraph()
+	subgraph.
 		Attr("label", "Description:").
 		Attr("color", "green").
 		Attr("fontcolor", "green").
 		Attr("style", "dashed")
-	descriptionNode := descriptionSubgraph.Node()
-	descriptionNode.
+	node := subgraph.Node()
+	node.
 		Attr("shape", "plaintext").
 		Attr("color", "green").
 		Attr("fontcolor", "green").
 		Attr("label", description)
+}
+
+// addCycleInfo adds useful insights about current cycle
+func addCycleInfo(graph *dot.Graph, activationCycle *cycle.Cycle, cycleNumber int) {
+	subgraph := graph.NewSubgraph()
+	subgraph.
+		Attr("label", "Cycle info:").
+		Attr("style", "dashed")
+	subgraph.NodeBaseAttrs().
+		Attr("shape", "plaintext")
+
+	// Current cycle number
+	cycleNumberNode := subgraph.Node()
+	cycleNumberNode.Attr("label", fmt.Sprintf("Current cycle: %d", cycleNumber))
+
+	// Stats
+	stats := getCycleStats(activationCycle)
+	statNode := subgraph.Node()
+	tableRows := dot.HTML("<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">")
+	for statName, statValue := range stats {
+		//@TODO: keep order
+		tableRows = tableRows + dot.HTML(fmt.Sprintf("<tr><td>%s : %d</td></tr>", statName, statValue))
+	}
+	tableRows = tableRows + "</table>"
+	statNode.Attr("label", tableRows)
+}
+
+// getCycleStats returns basic cycle stats
+func getCycleStats(activationCycle *cycle.Cycle) map[string]int {
+	stats := make(map[string]int)
+	for _, ar := range activationCycle.ActivationResults() {
+		if ar.Activated() {
+			stats["Activated"]++
+		}
+
+		stats[ar.Code().String()]++
+	}
+	return stats
 }
 
 // getPortID returns unique ID used to locate ports while building pipe edges
--- export/dot_test.go
+++ export/dot_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hovsep/fmesh"
 	"github.com/hovsep/fmesh/component"
 	"github.com/hovsep/fmesh/port"
+	"github.com/hovsep/fmesh/signal"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -68,6 +69,85 @@ func Test_dotExporter_Export(t *testing.T) {
 			exporter := NewDotExporter()
 
 			got, err := exporter.Export(tt.args.fm)
+			if tt.assertions != nil {
+				tt.assertions(t, got, err)
+			}
+		})
+	}
+}
+
+func Test_dotExporter_ExportWithCycles(t *testing.T) {
+	type args struct {
+		fm *fmesh.FMesh
+	}
+	tests := []struct {
+		name       string
+		args       args
+		assertions func(t *testing.T, data [][]byte, err error)
+	}{
+		{
+			name: "empty f-mesh",
+			args: args{
+				fm: fmesh.New("fm"),
+			},
+			assertions: func(t *testing.T, data [][]byte, err error) {
+				assert.NoError(t, err)
+				assert.Empty(t, data)
+			},
+		},
+		{
+			name: "happy path",
+			args: args{
+				fm: func() *fmesh.FMesh {
+					adder := component.New("adder").
+						WithDescription("This component adds 2 numbers").
+						WithInputs("num1", "num2").
+						WithOutputs("result").
+						WithActivationFunc(func(inputs port.Collection, outputs port.Collection) error {
+							num1 := inputs.ByName("num1").Signals().FirstPayload().(int)
+							num2 := inputs.ByName("num2").Signals().FirstPayload().(int)
+
+							outputs.ByName("result").PutSignals(signal.New(num1 + num2))
+							return nil
+						})
+
+					multiplier := component.New("multiplier").
+						WithDescription("This component multiplies number by 3").
+						WithInputs("num").
+						WithOutputs("result").
+						WithActivationFunc(func(inputs port.Collection, outputs port.Collection) error {
+							num := inputs.ByName("num").Signals().FirstPayload().(int)
+							outputs.ByName("result").PutSignals(signal.New(num * 3))
+							return nil
+						})
+
+					adder.Outputs().ByName("result").PipeTo(multiplier.Inputs().ByName("num"))
+
+					fm := fmesh.New("fm").
+						WithDescription("This f-mesh has just one component").
+						WithComponents(adder, multiplier)
+
+					adder.Inputs().ByName("num1").PutSignals(signal.New(15))
+					adder.Inputs().ByName("num2").PutSignals(signal.New(12))
+
+					return fm
+				}(),
+			},
+			assertions: func(t *testing.T, data [][]byte, err error) {
+				assert.NoError(t, err)
+				assert.NotEmpty(t, data)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			cycles, err := tt.args.fm.Run()
+			assert.NoError(t, err)
+
+			exporter := NewDotExporter()
+
+			got, err := exporter.ExportWithCycles(tt.args.fm, cycles)
 			if tt.assertions != nil {
 				tt.assertions(t, got, err)
 			}
