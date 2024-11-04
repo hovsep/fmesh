dbd1259cd8480c288e55ea7acc91b7e7b97d7c65
hovsep <avakyan.hovsep@yandex.ru>
2024-11-04 23:54:50 +0200

    Update readme

--- integration_tests/computation/basic_test.go
+++ integration_tests/computation/basic_test.go
@@ -1,12 +1,15 @@
 package integration_tests
 
 import (
+	"fmt"
 	"github.com/hovsep/fmesh"
 	"github.com/hovsep/fmesh/component"
 	"github.com/hovsep/fmesh/cycle"
 	"github.com/hovsep/fmesh/port"
 	"github.com/hovsep/fmesh/signal"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"strings"
 	"testing"
 )
 
@@ -69,4 +72,56 @@ func Test_Math(t *testing.T) {
 			tt.assertions(t, fm, cycles, err)
 		})
 	}
+}
+
+func Test_Readme(t *testing.T) {
+	t.Run("readme test", func(t *testing.T) {
+		fm := fmesh.New("hello world").
+			WithComponents(
+				component.New("concat").
+					WithInputs("i1", "i2").
+					WithOutputs("res").
+					WithActivationFunc(func(inputs *port.Collection, outputs *port.Collection) error {
+						word1 := inputs.ByName("i1").FirstSignalPayloadOrDefault("").(string)
+						word2 := inputs.ByName("i2").FirstSignalPayloadOrDefault("").(string)
+
+						outputs.ByName("res").PutSignals(signal.New(word1 + word2))
+						return nil
+					}),
+				component.New("case").
+					WithInputs("i1").
+					WithOutputs("res").
+					WithActivationFunc(func(inputs *port.Collection, outputs *port.Collection) error {
+						inputString := inputs.ByName("i1").FirstSignalPayloadOrDefault("").(string)
+
+						outputs.ByName("res").PutSignals(signal.New(strings.ToTitle(inputString)))
+						return nil
+					})).
+			WithConfig(fmesh.Config{
+				ErrorHandlingStrategy: fmesh.StopOnFirstErrorOrPanic,
+				CyclesLimit:           10,
+			})
+
+		fm.Components().ByName("concat").Outputs().ByName("res").PipeTo(
+			fm.Components().ByName("case").Inputs().ByName("i1"),
+		)
+
+		// Init inputs
+		fm.Components().ByName("concat").Inputs().ByName("i1").PutSignals(signal.New("hello "))
+		fm.Components().ByName("concat").Inputs().ByName("i2").PutSignals(signal.New("world !"))
+
+		// Run the mesh
+		_, err := fm.Run()
+
+		// Check for errors
+		if err != nil {
+			fmt.Println("F-Mesh returned an error")
+			os.Exit(1)
+		}
+
+		//Extract results
+		results := fm.Components().ByName("case").Outputs().ByName("res").FirstSignalPayloadOrNil()
+		fmt.Printf("Result is :%v", results)
+		assert.Equal(t, "HELLO WORLD !", results)
+	})
 }
