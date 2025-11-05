c9b1455a78f5324134db4fe951160cd28422c2d1
Ovsep Avakian <ovsep.avakian@sumup.com>
2025-11-05 03:41:15 +0200

    Fmesh regroup types

--- types.go
+++ types.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/hovsep/fmesh/cycle"
-	"github.com/hovsep/fmesh/hook"
 )
 
 // RuntimeInfo contains information about mesh execution.
@@ -13,19 +12,4 @@ type RuntimeInfo struct {
 	StartedAt time.Time
 	StoppedAt time.Time
 	Duration  time.Duration
-}
-
-// CycleContext provides context for cycle-level hooks.
-type CycleContext struct {
-	FMesh *FMesh
-	Cycle *cycle.Cycle
-}
-
-// Hooks is a registry of all hook types for FMesh.
-// All hooks are stored in typed groups and executed in insertion order.
-type Hooks struct {
-	beforeRun  *hook.Group[*FMesh]
-	afterRun   *hook.Group[*FMesh]
-	cycleBegin *hook.Group[*CycleContext]
-	cycleEnd   *hook.Group[*CycleContext]
 }
